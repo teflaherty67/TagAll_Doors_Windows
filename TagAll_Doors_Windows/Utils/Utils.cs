210c28c36afa5fe0ecdd62ac20193b86607fc372
Tom E Flaherty <teflaherty@live.com>
2023-07-10 12:02:27 -0500

    Add project files.

---
+++ TagAll_Doors_Windows/Utils/Utils.cs
@@ -0,0 +1,185 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace TagAll_Doors_Windows
+{
+    internal static class Utils
+    {
+
+        #region Ribbon
+        internal static RibbonPanel CreateRibbonPanel(UIControlledApplication app, string tabName, string panelName)
+        {
+            RibbonPanel currentPanel = GetRibbonPanelByName(app, tabName, panelName);
+
+            if (currentPanel == null)
+                currentPanel = app.CreateRibbonPanel(tabName, panelName);
+
+            return currentPanel;
+        }        
+
+        internal static RibbonPanel GetRibbonPanelByName(UIControlledApplication app, string tabName, string panelName)
+        {
+            foreach (RibbonPanel tmpPanel in app.GetRibbonPanels(tabName))
+            {
+                if (tmpPanel.Name == panelName)
+                    return tmpPanel;
+            }
+
+            return null;
+        }
+
+        #endregion
+
+        #region Tags
+
+        internal static FamilySymbol GetTagByName(Document curDoc, string tagName)
+        {
+            // get all loaded door & window tags
+            List<BuiltInCategory> m_colTags = new List<BuiltInCategory>();
+            m_colTags.Add(BuiltInCategory.OST_DoorTags);
+            m_colTags.Add(BuiltInCategory.OST_WindowTags);
+
+            ElementMulticategoryFilter filter = new ElementMulticategoryFilter(m_colTags);
+
+            IList<Element> tElements = new FilteredElementCollector(curDoc)
+                .WherePasses(filter)
+                .WhereElementIsElementType()
+                .ToElements();
+
+            foreach (Element elem in tElements)
+            {
+                if (elem.Name.Equals(tagName))
+                    return elem as FamilySymbol;
+            }
+
+            return null;
+        }
+
+        internal static void TagAllUntaggedDoorsInView(Document curDoc, View flrPlan)
+        {
+
+            // get all doors in the view
+            ICollection<ElementId> m_DoorIds = new FilteredElementCollector(curDoc, flrPlan.Id)
+                .OfCategory(BuiltInCategory.OST_Doors)
+                .WhereElementIsNotElementType()
+                .ToElementIds();
+
+            // filter out already tagged doors
+            ICollection<ElementId> m_UntaggedDoorIds = new List<ElementId>();
+            foreach (ElementId doorId in m_DoorIds)
+            {
+                // review Relationship Inverter page on The Building Coder
+
+                // I don't think this code is right
+
+                FamilyInstance door = curDoc.GetElement(doorId) as FamilyInstance;
+
+                if (door != null && door.GetParameters("Tag").FirstOrDefault() == null)
+                {
+                    m_UntaggedDoorIds.Add(doorId);
+                }
+            }
+
+            // set the door tag family & type
+            FamilySymbol doorTag = Utils.GetTagByName(curDoc, "Door Tag-Type Comments : Type 1");
+
+            // create a new tag for each untagged door
+            foreach (ElementId doorId in m_UntaggedDoorIds)
+            {
+                FamilyInstance door = curDoc.GetElement(doorId) as FamilyInstance;
+                
+                if (door != null)
+                {
+                    // create a reference for the door
+                    Reference doorRef = new Reference(door);
+
+                    // get the door location
+                    XYZ doorLoc = (door.Location as LocationPoint)?.Point;
+
+                    // create a new tag at the door location
+                    IndependentTag newTag = IndependentTag.Create(curDoc,
+                        doorTag.Id,
+                        flrPlan.Id,
+                        doorRef,
+                        false,
+                        TagOrientation.AnyModelDirection,
+                        doorLoc);
+
+                    // assign tag family symbol
+                    newTag.ChangeTypeId(doorTag.Id);
+                }
+            }            
+        }       
+
+        internal static void TagAllUntaggedWindowsInView(Document curDoc, View flrPlan)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void MarkAllDoorsInView(Document curDoc, View flrPlan)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void MarkAllWindowsInView(Document curDoc, View flrPlan)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void TagAllDoorOpeningsInView(Document curDoc, View dimPlan)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void TagAllWindowOpeningsInView(Document curDoc, View dimPlan)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Views
+
+        internal static List<View> GetAllViews(Document curDoc)
+        {
+            {
+                FilteredElementCollector m_colviews = new FilteredElementCollector(curDoc);
+                m_colviews.OfCategory(BuiltInCategory.OST_Views);
+
+                List<View> m_views = new List<View>();
+                foreach (View x in m_colviews.ToElements())
+                {
+                    if (x.IsTemplate == false)
+
+                        m_views.Add(x);
+                }
+
+                return m_views;
+            }
+        }
+
+        internal static List<View> GetAllViewsByNameContains(Document curDoc, string viewName)
+        {
+            List<View> m_Views = Utils.GetAllViews(curDoc);
+
+            List<View> m_ViewsToTag = new List<View>();
+
+            foreach (View curView in m_Views)
+            {
+                if (curView.Name.Contains(viewName))
+                    m_ViewsToTag.Add(curView);
+            }
+
+            return m_ViewsToTag;
+        }
+
+        #endregion
+
+    }
+}
---
+++ TagAll_Doors_Windows/cmdTagDrsWndws.cs
@@ -0,0 +1,112 @@
+#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+
+#endregion
+
+namespace TagAll_Doors_Windows
+{
+    [Transaction(TransactionMode.Manual)]
+    public class cmdTagDrsWndws : IExternalCommand
+    {
+        public Result Execute(
+          ExternalCommandData commandData,
+          ref string message,
+          ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document curDoc = uidoc.Document;
+
+            // put any code needed for the form here
+
+            // open form
+            frmTagDrsWndws curForm = new frmTagDrsWndws()
+            {
+                Width = 350,
+                Height = 200,
+                WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen,
+                Topmost = true,
+            };
+
+            curForm.ShowDialog();
+
+            // create some lists to hold the views
+            List<View> flrPlans = Utils.GetAllViewsByNameContains(curDoc, "Annotation");
+            List<View> dimPlans = Utils.GetAllViewsByNameContains(curDoc, "Dimension");
+
+            // tag variables
+
+            // tag parameters
+            TagMode tmode = TagMode.TM_ADDBY_CATEGORY;
+            TagOrientation tOrient = TagOrientation.Horizontal;
+
+            // get form data and do something
+
+            // create a transaction
+            using(Transaction trans = new Transaction(curDoc))
+            {
+                // start the transaction
+                trans.Start("Tag all doors and windows");
+
+                if (curForm.GetCheckBoxFlrs() == true)
+                {
+                    // loop through the annotation views
+                    foreach (View flrPlan in flrPlans)
+                    {
+                        // tag all doors in view
+                        Utils.TagAllUntaggedDoorsInView(curDoc, flrPlan);
+
+                        // tag all windows in view
+                        Utils.TagAllUntaggedWindowsInView(curDoc, flrPlan);
+                    }
+                }
+
+                if (curForm.GetCheckBoxDouble()  == true)
+                {
+                    // loop through the annotation views
+                    foreach (View flrPlan in flrPlans)
+                    {
+                        // mark all doors in view
+                        Utils.MarkAllDoorsInView(curDoc, flrPlan);
+
+                        // mark all windows in view
+                        Utils.MarkAllWindowsInView(curDoc, flrPlan);
+                    }
+                }
+
+                if (curForm.GetCheckBoxDims() == true)
+                {
+                    // loop through the dimension views
+                    foreach (View dimPlan in dimPlans)
+                    {
+                        // tag all door rough opening sizes
+                        Utils.TagAllDoorOpeningsInView(curDoc, dimPlan);
+
+                        // tag all window rough opening sizes
+                        Utils.TagAllWindowOpeningsInView(curDoc, dimPlan);
+                    }
+                }
+
+                // commit the transaction
+                trans.Commit();
+            }
+
+            return Result.Succeeded;
+        }
+
+        public static String GetMethod()
+        {
+            var method = MethodBase.GetCurrentMethod().DeclaringType?.FullName;
+            return method;
+        }
+    }
+}
